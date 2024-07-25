5aad83c982d4b9b610fcc66024d17ced0617b732
KartikSindura <kartik.sindura@gmail.com>
2024-07-25 23:18:51 +0530

    menu fix

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -23,8 +23,7 @@ const Menu = () => {
   };
   return (
     <div
-      className="w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4"
-      style={{ backgroundColor: colors.bg }}
+      className="w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4 bg-black"
     >
       {!user && (
         <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">
